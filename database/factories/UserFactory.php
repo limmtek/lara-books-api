6c40e70f904c03d7822e84bd3bf16b4d88a6347d
Igor Kulakov <kulakov.igor@mail.ru>
2020-01-06 22:23:14 +0300

    user seeder and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$iuFUvdCqo805lvYlTJFZ4uLZZimW0dePdKfER9WhD/2JPd4dqydEG', // 123
         'remember_token' => Str::random(10),
     ];
 });
