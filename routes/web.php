541282a60e2fd7ccbd5250a60f66069980e1f3eb
Igor Kulakov <kulakov.igor@mail.ru>
2020-01-07 23:25:40 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/books', 'BookController');
+Route::resource('/books', 'BookController', ['only' => ['index', 'create', 'store', 'edit', 'update', 'destroy']]);
